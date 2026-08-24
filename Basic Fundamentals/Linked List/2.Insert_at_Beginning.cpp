542e50608f0472909c6164caf2e33b16604675bd
Ayan Banerjee <150042334+Ayan0106@users.noreply.github.com>
2026-08-24 13:18:57 +0530

    Update 2.Insert_at_Beginning.cpp

--- Basic Fundamentals/Linked List/2.Insert_at_Beginning.cpp
+++ Basic Fundamentals/Linked List/2.Insert_at_Beginning.cpp
@@ -1,42 +1,62 @@
 // 5
-// 5 10 20 30 
-#include<bits/stdc++.h>
+// 5->10->20->NULL
+// 30
+// 5->10->20->30->NULL
+
+#include <bits/stdc++.h>
 using namespace std;
 struct Node{
-    int data;
-    Node* ptr;
-    Node(int value){
-        data=value;
-        ptr=nullptr;
-    }
+  int data;
+  Node* ptr;
+  Node(int value){
+      data=value;
+      ptr= nullptr;
+  }
 };
+
 Node* insertbeg(Node* head, int value){
     Node* newnode=new Node(value);
     newnode->ptr=head;
     head=newnode;
     return head;
 }
-void display(Node* head) {
-   Node* temp=head;
+Node* insertend(Node* head, int value){
+    Node* newnode=new Node(value);
+    
+    Node* temp=head;
+    while(temp->ptr!=nullptr){
+        temp=temp->ptr;
+    }
+    temp->ptr=newnode;
+    return head;
+}
+
+void display(Node* head){
+    Node* temp=head;
     while(temp!=nullptr){
-        cout<<temp->data<<" ";
+        cout<<temp->data<<"->";
         temp=temp->ptr;
         if(temp==nullptr){
             cout<<"NULL";
         }
     }
 }
-int main(){
+int main (){
     Node* head=new Node(10);
     head->ptr=new Node(20);
-    head->ptr->ptr=new Node(30);
-    head->ptr->ptr->ptr=nullptr;
-
-    int value;
-    cin>>value;
-    
-    head= insertbeg(head, value);
+    head->ptr->ptr=nullptr;
+    // insertion at the beg.
+    int n;
+    cin>>n;
+    head=insertbeg(head, n);
     display(head);
+    
+    cout<<endl;
+    // insertion at the end.
+    int n2;
+    cin>>n2;
+    head=insertend(head, n2);
+    display(head);
+    
     return 0;
 }
-
